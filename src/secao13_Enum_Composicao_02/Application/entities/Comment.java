264868f7a6f2fe490d9711c6a40ce1c84e298c2d
Jean <jesilva456@hotmail.com>
2022-01-29 20:10:19 -0300

    Exercicio composicao 02

---
+++ src/secao13_Enum_Composicao_02/Application/entities/Comment.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secao13_Enum_Composicao_02.Application.entities;
+
+/**
+ *
+ * @author Jean
+ */
+public class Comment {
+    private String text;
+
+    public Comment() {
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    
+}
