ec1ed328c4c23045c34fd582bd636851ee4b8e46
Jean <jesilva456@hotmail.com>
2022-01-29 19:17:02 -0300

    Finalizando ex Enum/Composicao

---
+++ src/secao13_Enum_Composicao/entities/Worker.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secao13_Enum_Composicao.entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import secao13_Enum_Composicao.enums.WorkerLevel;
+
+/**
+ *
+ * @author Jean
+ */
+public class Worker {
+
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalary;
+    private Department department;
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker() {
+    }
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(List<HourContract> contracts) {
+        this.contracts = contracts;
+    }
+    
+    
+
+    public void addContract(HourContract contract) {
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract) {
+        contracts.remove(contract);
+    }
+
+    public double income(Integer year, Integer month) {
+        double sum = baseSalary;
+        Calendar cal = Calendar.getInstance();
+        for (HourContract c : contracts) {
+            cal.setTime(c.getDate());
+            int c_year = cal.get(Calendar.YEAR);
+            int c_month = 1 + cal.get(Calendar.MONTH);
+            if (year == c_year && month == c_month) {
+                sum += c.totalValue();
+            }
+        }
+        return sum;
+
+    }
+
+    @Override
+    public String toString() {
+        for (HourContract hc : contracts) {
+            System.out.println(hc);
+        }
+
+        return name
+                + ", "
+                + level
+                + ", "
+                + baseSalary
+                + ", "
+                + department.getName();
+
+    }
+
+}
