62564054804c667f384e065b88230b1304d18e9c
Jean <jesilva456@hotmail.com>
2022-02-03 22:01:09 -0300

    secao18-Interface-Exercico01

---
+++ src/secao18_Interfaces/model/services/BrazilTaxService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secao18_Interfaces.model.services;
+
+/**
+ *
+ * @author Jean
+ */
+public class BrazilTaxService implements TaxService{
+    public double tax(double amount){
+        if(amount <= 100){
+            return amount * 0.2;
+        }
+        else {
+            return amount * 0.15;
+        }
+    }
+    
+}
---
+++ src/secao18_Interfaces/model/services/TaxService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secao18_Interfaces.model.services;
+
+/**
+ *
+ * @author Jean
+ */
+public interface TaxService {
+    double tax(double amount);
+    
+}
